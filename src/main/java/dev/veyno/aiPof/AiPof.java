2ecbac56b8f02f026fdea2b109ba32635e467975
veyn0 <veynoplugins@gmail.com>
2026-01-25 00:20:33 +0100

    wasd

---
+++ src/main/java/dev/veyno/aiPof/AiPof.java
@@ -0,0 +1,17 @@
+package dev.veyno.aiPof;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class AiPof extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
